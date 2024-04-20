1f659deab2934f4a796a4419cb5b3ea5db6adfd7
German Torres <gtf@grisas.co>
2024-04-20 11:41:07 -0500

    correct

--- flutter/lib/mobile/pages/server_page.dart
+++ flutter/lib/mobile/pages/server_page.dart
@@ -541,8 +541,8 @@ class _PermissionCheckerState extends State<PermissionChecker> {
               translate("Screen Capture"),
               serverModel.mediaOk,
               !serverModel.mediaOk &&
-                      gFFI.userModel.userName.value.isEmpty 
-                  : serverModel.toggleService),
+                      gFFI.userModel.userName.value.isEmpty, 
+                   serverModel.toggleService),
           PermissionRow(translate("Input Control"), serverModel.inputOk,
               serverModel.toggleInput),
           PermissionRow(translate("Transfer file"), serverModel.fileOk,
